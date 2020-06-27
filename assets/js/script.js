1af7931b5f649aab0ae246bfc8ce6f8593e78fe1
Jacob Daughtry <akutozo@gmail.com>
2020-06-27 13:10:10 -0500

    Setting up base files

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+//Establish variables for the course to run efficiently.
+
+//Provide a user interface that is fair and challenging.
+
+//Reward users who successfully navigate these chellenges.
+
+//Give only what is deserving to those who do not.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
