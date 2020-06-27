00c5a1ff90b7d761108d3edc110f38c99e5c9506
Jacob Daughtry <akutozo@gmail.com>
2020-06-27 13:41:45 -0500

    Basic User Interface

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
+var timerEl = document.getElementById('countdown');
+var startBtn = document.getElementById('start_game');
+
+function countdown() {
+    var timeLeft = 100;
+    var timeInterval = setInterval(function() {
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
 
 
 
-
-
-
-
+startBtn.onclick = countdown;
 
 
 
